9a524bd5290c96fa50f9a0312977bd5c6675199c
Turner Millsaps <turnermillsaps.plp@gmail.com>
2019-12-18 10:47:14 -0500

    Initial commit

---
+++ scripts/main.js
@@ -0,0 +1,6 @@
+// querySelector() grabs the referenced element in the HTML document to apply the javascript code to 
+// we stored this in the variable myHeading 
+// we then set myHeading variable textContent to 'Hello World'
+
+const myHeading = document.querySelector('h1');
+myHeading.textContent = 'Hello world!';
